5f41d5637446214694c0b858bc1f66ecfd7d85aa
udikabudi <udizohar84@gmail.com>
2014-02-27 08:52:15 -0500

    adding log

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
+    
+    def my_logger
+    @@my_logger ||= Logger.new("#{Rails.root}/log/my.log")
+  end
+  
   def home
+      my_logger.info("home")
   end
 
   def help
